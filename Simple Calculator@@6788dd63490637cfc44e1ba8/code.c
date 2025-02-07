47a6b7f4f0461e7ab3d1240d23ce7d6bf433da98
wakeupsidd30 <siddhantrajpal2005@gmail.com>
2025-02-07 18:15:18 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,20 +3,33 @@
 int main() {
     int a, b;
     char op;
-    scanf("%d %d %c", &a, &b, &op); 
+
+    scanf("%d %d %c", &a, &b, &op);
 
     switch (op) {
-        case '+': printf("%d\n", a+b); break;
-        case '+': printf("%d\n", a+b); break;
-        case '+': printf("%d\n", a+b); break;
+        case '+': 
+            printf("%d\n", a + b); 
+            break; // Essential to exit the 'case +' block
+
+        case '-': 
+            printf("%d\n", a - b); 
+            break; // Essential to exit the 'case -' block
+
+        case '*': 
+            printf("%d\n", a * b); 
+            break; // Essential to exit the 'case *' block
+
         case '/':
-            if (b == 0) printf("Error: Division by zero\n");
-            else printf("%d\n", a / b); 
-            break;
-        default: printf("Error: Invalid operator\n");
+            if (b == 0) {
+                printf("Error: Division by zero\n");
+            } else {
+                printf("%d\n", a / b);
+            }
+            break; // Essential to exit the 'case /' block
+
+        default: 
+            printf("Error: Invalid operator\n");
     }
 
     return 0;
 }
-
- 
