3a170e13031330c0e20d27ee48ae492d75500856
wakeupsidd30 <siddhantrajpal2005@gmail.com>
2025-03-27 18:43:31 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,1 +1,16 @@
-// Your code here...
+def balloon_deflation_game(balloons):
+    balloons.sort()
+    prev_sub = 0
+    result = []
+    n = len(balloons)
+    for i in range(n):
+        if balloons[i] <= prev_sub:
+            continue
+        current_min = balloons[i] - prev_sub
+        result.append(n - i)
+        prev_sub += current_min
+    return result
+
+# Example usage:
+balloons = [5, 4, 6, 3]
+print(balloon_deflation_game(balloons))  # Output: [4, 3, 2, 1]
