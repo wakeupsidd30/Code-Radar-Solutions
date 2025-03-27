f05a04a18c5a7bb31b3639a4c7402a4e7d10b3ce
wakeupsidd30 <siddhantrajpal2005@gmail.com>
2025-03-27 19:05:00 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to encrypt the message using Caesar Cipher
+
 void caesarCipher(char *message, int shift) {
-    // Normalize the shift value to be within 0-25
+   
     shift = shift % 26;
     
     for (int i = 0; message[i] != '\0'; i++) {
         char currentChar = message[i];
 
-        // Encrypt uppercase letters
+        
         if (currentChar >= 'A' && currentChar <= 'Z') {
             message[i] = (currentChar - 'A' + shift) % 26 + 'A';
         }
-        // Encrypt lowercase letters
+       
         else if (currentChar >= 'a' && currentChar <= 'z') {
             message[i] = (currentChar - 'a' + shift) % 26 + 'a';
         }
-        // Non-alphabetic characters remain unchanged
+        
     }
 }
 
